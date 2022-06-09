c52674d3b7f11e20a7e1547ada2aa8b0b292890e
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-06-09 16:34:31 -0400

    removed parts of chakra

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next';
-import { Box } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
 	return (
-		<Box color="white">
+		<div>
 			<h1>Hello, World</h1>
-		</Box>
+		</div>
 	);
 };
 
